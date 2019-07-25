2bcc979d338f3de1a3c2ce1deb1ba737499a13a4
Nikit <03nikit72@gmail.com>
2019-07-25 23:37:10 +0600

    lesson_3(functions)

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -1,19 +1,7 @@
-/*var ds = 1,
-    sum = 0,
-    num = prompt('Number ?');
-
-for (let i = 0; i < num.length; i++){
-    sum += +num[i];
-    ds *= +num[i]
-}
-
-console.log(sum);
-console.log(ds);
-console.log(Math.pow(ds,3));*/
-
-let money = +prompt('Ваш бюджет ?');
-let name = prompt('Имя вашего магазина ?');
-let time = 22;
+let money,
+    name,
+    time,
+    price = 1000;
 
 mainList = {
     budget: money,
@@ -21,30 +9,70 @@ mainList = {
     shopGoods: [],
     employers: {},
     open: false,
+    discount: true,
 }
-for (let i = 0; i < 5; i++){
 
-     let a = prompt('Какой тип товара будем продавать ?');
+let start = () =>{
+    money = +prompt('Ваш бюджет ?');
+    while (isNaN(money) || money== "" || money == null){
+        money = +prompt('Ваш бюджет ?');
+    }
+    name = prompt('Имя вашего магазина ?').toUpperCase();
+    time = 22;
+}
 
-    if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50){
-        mainList.shopGoods[i] = a;
-        console.log('Good');
-    } else {
-        i--;
+let chooseGoods = () =>{
+    for (let i = 0; i < 5; i++){
+
+        let a = prompt('Какой тип товара будем продавать ?');
+
+        if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50){
+            mainList.shopGoods[i] = a;
+            console.log('Good');
+        } else {
+            i--;
+        }
     }
 }
 
-if (time < 0){
-    console.log('Такого просто не может быть !');
-} else if (time > 8 && time < 20){
-    console.log('Время работать !');
+let workTime = (time) =>{
+    if (time < 0){
+        console.log('Такого просто не может быть !');
+    } else if (time > 8 && time < 20){
+        console.log('Время работать !');
     }else if (time > 21 && time < 24){
         console.log('Уже слишком поздно !');
-        } else {
-            console.log('В сутках только 24 часа');
-            }
+    } else {
+        console.log('В сутках только 24 часа');
+    }
+}
 
-alert ('Ваш бюджет на день ' + mainList.budget / 30);
+let dayBudget = (money) =>{
+    mainList.budget = money/30;
+    alert('Ваш бюджет на день ' + mainList.budget);
+}
+
+let checkDiscount = (price, discount) => {
+    if (mainList.discount == true){
+        price = price*80/100;
+    }
+    console.log(price);
+}
+
+let hireEmployers = () =>{
+    for (let i = 1; i < 5; i++){
+        mainList.employers[i] =prompt('Введитя имя и телефон нового сотрудника в формате Номер - Имя');
+    }
+}
+
+// start();
+// chooseGoods();
+// workTime(15);
+// dayBudget(money);
+// checkDiscount(price, mainList.discount);
+//hireEmployers();
+
+
 
 console.log(mainList);
 
