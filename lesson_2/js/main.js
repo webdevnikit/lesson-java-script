d40ffe0e5fa010cfe98ad2eaf8423b754ab50737
Nikit <03nikit72@gmail.com>
2019-07-26 02:20:10 +0600

    lesson_4 Array and Object

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -4,65 +4,82 @@ let money,
     price = 1000;
 
 mainList = {
-    budget: money,
-    shopName: name,
-    shopGoods: [],
-    employers: {},
-    open: false,
-    discount: true,
-}
-
-let start = () =>{
-    money = +prompt('Ваш бюджет ?');
-    while (isNaN(money) || money== "" || money == null){
+    budget      : money,
+    shopName    : name,
+    shopGoods   : [],
+    employers   : {},
+    open        : false,
+    discount    : true,
+    shopItem    : [],
+    start : function start (){
         money = +prompt('Ваш бюджет ?');
-    }
-    name = prompt('Имя вашего магазина ?').toUpperCase();
-    time = 22;
-}
-
-let chooseGoods = () =>{
-    for (let i = 0; i < 5; i++){
-
-        let a = prompt('Какой тип товара будем продавать ?');
-
-        if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50){
-            mainList.shopGoods[i] = a;
-            console.log('Good');
-        } else {
-            i--;
+        while (isNaN(money) || money== "" || money == null){
+            money = +prompt('Ваш бюджет ?');
         }
-    }
-}
+        name = prompt('Имя вашего магазина ?').toUpperCase();
+        time = 22;
+    },
+    chooseGoods : function chooseGoods(){
+        for (let i = 0; i < 5; i++){
+            let a = prompt('Какой тип товара будем продавать ?');
+            if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50){
+                mainList.shopGoods[i] = a;
+                console.log('Good');
+            } else {
+                i--;
+            }
+        }
+    },
+    workTime : function workTime(time){
+        if (time < 0){
+            console.log('Такого просто не может быть !');
+        } else if (time > 8 && time < 20){
+            console.log('Время работать !');
+            mainList.open = true;
+        }else if (time > 21 && time < 24){
+            console.log('Уже слишком поздно !');
+        } else {
+            console.log('В сутках только 24 часа');
+        }
+    },
+    dayBudget : function dayBudget(money){
+        mainList.budget = money/30;
+        alert('Ваш бюджет на день ' + mainList.budget);
+    },
+    checkDiscount : function checkDiscount(price, discount){
+        if (mainList.discount == true){
+            price = price*80/100;
+        }
+        console.log(price);
+    },
+    hireEmployers : function hireEmployers(){
+        for (let i = 1; i < 5; i++){
+            mainList.employers[i] =prompt('Введитя имя и телефон нового сотрудника в формате Номер - Имя');
+        }
+    },
+    chooseShopItem : function chooseShopItem(){
+         let item = prompt("Перечислети товары через запятую которые будем продавать",'');
+         while (typeof(item) === null || item == '' || typeof(item) !== 'string' ){
+             item = prompt("Перечислети товары через запятую которые будем продавать",'');
+         }
+         mainList.shopItem = item.split(',');
+         mainList.shopItem.push(prompt('Подождите, еще ?',''));
+         mainList.shopItem.sort();
 
-let workTime = (time) =>{
-    if (time < 0){
-        console.log('Такого просто не может быть !');
-    } else if (time > 8 && time < 20){
-        console.log('Время работать !');
-    }else if (time > 21 && time < 24){
-        console.log('Уже слишком поздно !');
-    } else {
-        console.log('В сутках только 24 часа');
-    }
-}
-
-let dayBudget = (money) =>{
-    mainList.budget = money/30;
-    alert('Ваш бюджет на день ' + mainList.budget);
-}
-
-let checkDiscount = (price, discount) => {
-    if (mainList.discount == true){
-        price = price*80/100;
-    }
-    console.log(price);
-}
-
-let hireEmployers = () =>{
-    for (let i = 1; i < 5; i++){
-        mainList.employers[i] =prompt('Введитя имя и телефон нового сотрудника в формате Номер - Имя');
-    }
+    },
+    writeShopItem : function writeShopItem() {
+        let l
+        mainList.shopItem.forEach(function (item,i) {
+            l = i +1;
+            alert('У нас вы можете купить '+ l +' : '+item);
+        })
+    },
+    writeSystem : function writeSystem(){
+        console.log('Наш магазтн включает в себя');
+        for (let key in mainList){
+            console.log(key);
+        }
+    },
 }
 
 // start();
@@ -71,6 +88,8 @@ let hireEmployers = () =>{
 // dayBudget(money);
 // checkDiscount(price, mainList.discount);
 //hireEmployers();
+
+
 
 
 
